9c74fbbcf00ab54cb24b73e9e10845070bcb070c
Devashish Priyadarshi <devashish234073@gmail.com>
2024-08-03 23:30:05 +0530

    getting model name from server.js

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { exec } = require('child_process');
+
+models = [];
+function populateModels() {
+    exec('ollama list', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing command: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Error: ${stderr}`);
+            return;
+        }
+        const lines = stdout.split('\n');
+        for (let i = 1; i < lines.length; i++) {
+            const line = lines[i].trim();
+            if (line) {
+                const parts = line.split(/\s+/);
+                const modelName = parts[0];
+                models.push(modelName);
+            }
+        }
+        console.log("available models ",models);
+    });
+}
+if(process.argv[2]) {
+  models.push(process.argv[2]);
+} else {
+  populateModels();
+}
 
 const app = express();
 const PORT = 9999;
-const MODEL_NAME = process.argv[2];
-console.log("running against model ",MODEL_NAME);
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/models',(req,res)=>{
+  res.end(JSON.stringify(models));
+});
+
 app.post('/generate', (req, res) => {
   const prompt = req.body.prompt;
+  const MODEL = req.body.model;
   const postData = JSON.stringify({
-    model: MODEL_NAME,
+    model: MODEL,
     prompt: prompt
   });
 
